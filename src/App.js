12cbd2542fc36344cc9b5313d27590a66fa8321e
VlasovaAlexsandra <68018046+VlasovaAlexsandra@users.noreply.github.com>
2021-04-06 00:20:35 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+
+
+class App extends Component {
+  state = {
+
+  }
+
+  render() {
+
+    return (
+      <>
+        <h1>Image Finder</h1>
+
+      </>
+
+    )
+  }
+}
+
+export default App;
