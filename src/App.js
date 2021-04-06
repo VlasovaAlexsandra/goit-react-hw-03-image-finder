7ad826349a1423a45cb9c4c7164806b2ec084d5a
VlasovaAlexsandra <68018046+VlasovaAlexsandra@users.noreply.github.com>
2021-04-06 03:34:21 +0300

    add api

--- src/App.js
+++ src/App.js
@@ -1,17 +1,85 @@
 import React, { Component } from 'react';
+import Searchbar from './components/Searchbar/Searchbar'
+import hitsApi from './services/hits-api'
+import './styles.css'
 
-
+// // const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${apiKey}`;
+// axios.defaults.headers.common['Authorization'] =
+//   '20282142-3926486b0d0a2f754919f4d11';
 
 class App extends Component {
   state = {
+    hits: [],
+    currentPage: 1,
+    searchQuery: '',
+    isLoading: false,
+    error: null,
+    largeImageURL: ''
+
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.searchQuery !== this.state.searchQuery) {
+      this.fetchHits()
+    }
+  }
+
+  onChangeQuery = query => {
+    this.setState({
+      searchQuery: query,
+      currentPage: 1,
+      hits: [],
+      error: null
+    })
+
+  }
+
+  fetchHits = () => {
+    const { currentPage, searchQuery } = this.state
+
+    const options = { searchQuery, currentPage }
+
+    this.setState({ isLoading: true })
+
+    hitsApi.fetchHits(options)
+      .then(hits => {
+        // console.log(response.data.hits)
+        this.setState(prevState => ({
+          hits: [...prevState.hits, ...hits],
+          currentPage: prevState.currentPage + 1
+        }))
+      }).catch(error => this.setState({ error }))
+      .finally(() => this.setState({ isLoading: false }))
 
   }
 
   render() {
+    const { hits, isLoading, error } = this.state
+    const shouldRenderLoadMoreButton = hits.length > 0 && !isLoading
 
     return (
+
       <>
-        <h1>Image Finder</h1>
+        {error && <h1>Ooooops....ERROR</h1>}
+        <Searchbar onSubmit={this.onChangeQuery} />
+
+        <ul className="ImageGallery">
+          {hits.map(({ id, webformatURL, tags }) => (
+            <li key={id} className="ImageGalleryItem">
+              <img src={webformatURL} alt={tags} width="450" className="ImageGalleryItem-image"></img>
+            </li>
+          ))}
+        </ul>
+
+        {isLoading && <h1>Loading...</h1>}
+
+        {shouldRenderLoadMoreButton && (
+          <button
+            type="button"
+            onClick={this.fetchHits}
+          >Load more</button>
+        )}
+
 
       </>
 
---
+++ src/components/Searchbar/Searchbar.js
@@ -0,0 +1,47 @@
+import { Component } from "react";
+
+
+class Searchbar extends Component {
+    state = { query: '' };
+
+    handleChange = e => {
+        this.setState({ query: e.currentTarget.value })
+    }
+
+    handleSubmit = e => {
+        e.preventDefault()
+        // console.log(this.state);
+        this.props.onSubmit(this.state.query)
+        this.setState({ query: '' })
+    }
+
+
+
+    render() {
+        return (
+            <header className="Searchbar">
+                <form
+                    className="SearchForm"
+                    onSubmit={this.handleSubmit}
+                >
+
+                    <button type="submit" className="SearchForm-button">
+                        <span className="SearchForm-button-label">Search</span>
+                    </button>
+
+                    <input
+                        className="SearchForm-input"
+                        type="text"
+                        value={this.state.query}
+                        // autocomplete="off"
+                        // autofocus
+                        placeholder="Search images and photos"
+                        onChange={this.handleChange}
+                    />
+                </form>
+            </header>
+        )
+    }
+}
+
+export default Searchbar
